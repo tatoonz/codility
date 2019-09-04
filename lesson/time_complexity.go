35532b9fbc7551c1a6196fdc17001f07db8c331e
TaToonz <peerapath.t@gmail.com>
2019-09-04 22:59:13 +0700

    lesson: add solution for PermMissingElem

--- lesson/time_complexity.go
+++ lesson/time_complexity.go
@@ -9,3 +9,17 @@ func FrogJmp(X, Y, D int) int {
 	result := math.Ceil(float64(Y-X) / float64(D))
 	return int(result)
 }
+
+// PermMissingElem finds the missing element in a given permutation.
+func PermMissingElem(A []int) int {
+	n := len(A) + 1
+
+	expectedTotal := n * (n + 1) / 2
+
+	total := 0
+	for i := range A {
+		total += A[i]
+	}
+
+	return expectedTotal - total
+}
--- lesson/time_complexity_test.go
+++ lesson/time_complexity_test.go
@@ -12,3 +12,9 @@ func TestFrogJmp(t *testing.T) {
 	assert.Equal(t, 3, lesson.FrogJmp(10, 85, 30))
 	assert.Equal(t, 34, lesson.FrogJmp(5, 105, 3))
 }
+
+func TestPermMissingElem(t *testing.T) {
+	assert.Equal(t, 4, lesson.PermMissingElem([]int{2, 3, 1, 5}))
+	assert.Equal(t, 1, lesson.PermMissingElem([]int{}))
+	assert.Equal(t, 2, lesson.PermMissingElem([]int{1}))
+}
